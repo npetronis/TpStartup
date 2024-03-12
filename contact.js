d7828c750fe40de67b6846c3e1406ab6fb947d94
npetronis <144010678+npetronis@users.noreply.github.com>
2024-03-12 15:53:33 +0100

    je sais pa sc quoi les modifs mais OK

--- contact.js
+++ contact.js
@@ -1,18 +1,19 @@
-const inputs = document.querySelectorAll(".input");
+// Sélectionnez tous les champs d'entrée du formulaire
+const inputFields = document.querySelectorAll('.input');
 
-function focusFunc() {
-  let parent = this.parentNode;
-  parent.classList.add("focus");
-}
+// Boucle sur chaque champ d'entrée pour ajouter des écouteurs d'événements
+inputFields.forEach(inputField => {
+    // Ajoute un écouteur d'événements pour l'événement "input" (lorsque l'utilisateur tape du texte)
+    inputField.addEventListener('input', () => {
+        // Efface le texte par défaut lorsqu'un utilisateur commence à taper dans un champ d'entrée
+        inputField.nextElementSibling.classList.add('active');
+    });
 
-function blurFunc() {
-  let parent = this.parentNode;
-  if (this.value == "") {
-    parent.classList.remove("focus");
-  }
-}
-
-inputs.forEach((input) => {
-  input.addEventListener("focus", focusFunc);
-  input.addEventListener("blur", blurFunc);
+    // Ajoute un écouteur d'événements pour l'événement "blur" (lorsque le champ d'entrée perd le focus)
+    inputField.addEventListener('blur', () => {
+        // Vérifie si le champ d'entrée est vide, puis masque le texte par défaut si c'est le cas
+        if (inputField.value === '') {
+            inputField.nextElementSibling.classList.remove('active');
+        }
+    });
 });
